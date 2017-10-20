22675b7826da52f461a0c6cec06de3d0209548b7
asabagh <a.sabagh72@gmail.com>
2017-10-20 18:55:11 +0330

    adding nestable simple handle right align with persian ci encoding character

---
+++ Nestable/Simple/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    //init nestable
+    var nestable_list = $('#nestable').nestable();
+
+    // update nestable function
+    function updateOutput(){
+        if(window.JSON){
+            $("#nestable-output").val(window.JSON.stringify(nestable_list.nestable("serialize")));
+        }else{
+            alert("Your Browser Does't Support JSON");
+        }
+    }
+    //call updateOutput after_load change
+    nestable_list.on('change', updateOutput);
+    updateOutput();
+});
