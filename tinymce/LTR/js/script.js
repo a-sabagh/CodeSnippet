f0fc3d3ec0f2940b9b24a7427724cf5a59c16b1b
asabagh <a.sabagh72@gmail.com>
2017-11-20 22:02:46 +0330

    adding ltr en_US tinymce

---
+++ tinymce/LTR/js/script.js
@@ -0,0 +1,9 @@
+tinymce.init({
+    selector: 'textarea.tinymce',
+    height: 500,
+    theme: 'modern',
+    plugins: "table , link , lists , hr , paste , print , textcolor , code , emoticons , searchreplace , spellchecker , directionality",
+    toolbar1: "undo redo | styleselect | fontsizeselect | searchreplace spellchecker | bold italic | emoticons hr | link unlink | forecolor backcolor | alignleft aligncenter alignright alignjustify | ltr rtl | bullist numlist outdent indent ",
+    //toolbar2: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+});
+  
