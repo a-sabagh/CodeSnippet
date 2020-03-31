1ce6f7aa356cfb5a5d7f3df8f3ba5bef30ee220b
a-sabagh <a.sabagh72@gmail.com>
2020-04-01 04:02:09 +0430

    slick slider with navigation compatible magnifier

---
+++ slick-nav-zoom/js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){ 
+	$(".rng-zoom").magnify({
+		speed: 300
+	});
+	/*
+	https://github.com/kenwheeler/slick#settings
+	*/
+	$('.slider').slick({
+		adaptiveHeight: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: false,
+		asNavFor: '.slider-nav-thumbnails',
+		draggable: false,
+		swipe: false,
+		touchMove: false,
+		swipeToSlide: false
+
+	});
+
+	$('.slider-nav-thumbnails').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		asNavFor: '.slider',
+		arrows: false,
+		focusOnSelect: true,
+		responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
+
+
+});
